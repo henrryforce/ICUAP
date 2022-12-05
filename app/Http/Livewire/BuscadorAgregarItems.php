a2d6e96b1cb03ce0a434df9c6a7a98c801dc701b
henrryforce <luis15ago98@gmail.com>
2022-12-04 21:23:40 -0600

    buscador dinamico primeros pasos falta css

--- app/Http/Livewire/BuscadorAgregarItems.php
+++ app/Http/Livewire/BuscadorAgregarItems.php
@@ -3,11 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Investigadore;
 
 class BuscadorAgregarItems extends Component
 {
+    public $buscar='';
+    public $investigadores=[];
+    public $piecked=false;
+    public $investigador;
     public function render()
     {
         return view('livewire.buscador-agregar-items');
+    }
+    public function updatedBuscar(){
+        $this->validate([
+            "buscar" => "required|min:2"
+        ]);
+        $this->investigadores = Investigadore::where('apellido_paterno','like',trim($this->buscar).'%')->take(3)->get();
     }
 }
