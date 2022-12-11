4248b067448ab972eab9a3dd53f73320be3f7983
henrryforce <luis15ago98@gmail.com>
2022-12-10 19:14:49 -0600

    validacion adicional de proteccion sin permisos de administracion

--- app/Http/Controllers/PaginaListaUsuarios.php
+++ app/Http/Controllers/PaginaListaUsuarios.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PaginaListaUsuarios extends Controller
 {
     public function __construct(){
         $this->middleware('auth');
+        
     } 
     public function index(){
+        $user = Auth::user();
+        if($user->tipo_user !=1){
+            return back();
+        }
         return view('lista_usuarios');
     }
     // public function destroy(Request $request){
